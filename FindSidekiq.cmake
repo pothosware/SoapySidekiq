e821c63a5bd4a07c190eb5edb0758dc19c18c715
Dave Helm <dave.helm@epiq-solutions.com>
2022-04-20 16:23:35 -0500

    remove the epiq directories

--- FindSidekiq.cmake
+++ FindSidekiq.cmake
@@ -21,40 +21,27 @@ if(NOT Sidekiq_FOUND)
 
     if(${outVar} MATCHES "x86_64")
         set (libname  "libsidekiq__x86_64.gcc.a")
-        set (epiq_name "~/sidekiq_sdk_current/lib/support/x86_64.gcc/usr/lib/epiq")
     else()
         set(libname  "libsidekiq__aarch64.gcc6.3.a")
-        set (epiq_name "~/sidekiq_sdk_current/lib/support/aarch64.gcc6.3/usr/lib/epiq")
     endif()
 
-    message(STATUS "library is ${libname} Epiq lib is ${epiq_name}")
+    message(STATUS "library is ${libname} ")
 
     find_library(Sidekiq_LIBRARY
         NAMES ${libname}
         HINTS ${Sidekiq_PKG_LIBRARY_DIRS} $ENV{Sidekiq_DIR}/include
         PATHS ~/sidekiq_sdk_current/lib/ /usr/local/lib /usr/lib /opt/lib /opt/local/lib)
 
-    find_library(Sidekiq_EPIQ_USB
-        NAMES libusb-1.0.so
-        HINTS ${Sidekiq_PKG_LIBRARY_DIRS} $ENV{Sidekiq_DIR}/include
-        PATHS ${epiq_name} /usr/local/lib /usr/lib /opt/lib /opt/local/lib)
-
-    find_library(Sidekiq_EPIQ_GLIB
-        NAMES libglib-2.0.so
-        HINTS ${Sidekiq_PKG_LIBRARY_DIRS} $ENV{Sidekiq_DIR}/include
-        PATHS ${epiq_name} /usr/local/lib /usr/lib /opt/lib /opt/local/lib)
 
     set(Sidekiq_LIBRARIES ${Sidekiq_LIBRARY})
     set(Sidekiq_INCLUDE_DIRS ${Sidekiq_INCLUDE_DIR})
-    set(Sidekiq_EPIQ_USB ${Sidekiq_EPIQ_USB})
-    set(Sidekiq_EPIQ_GLIB ${Sidekiq_EPIQ_GLIB})
 
     include(FindPackageHandleStandardArgs)
     # handle the QUIETLY and REQUIRED arguments and set LibSidekiq_FOUND to TRUE
     # if all listed variables are TRUE
     find_package_handle_standard_args(Sidekiq  DEFAULT_MSG
-        Sidekiq_LIBRARY Sidekiq_INCLUDE_DIR Sidekiq_EPIQ_USB Sidekiq_EPIQ_GLIB)
+        Sidekiq_LIBRARY Sidekiq_INCLUDE_DIR )
 
-    mark_as_advanced(Sidekiq_INCLUDE_DIR Sidekiq_LIBRARY Sidekiq_EPIQ_USB Sidekiq_EPIQ_GLIB)
+    mark_as_advanced(Sidekiq_INCLUDE_DIR Sidekiq_LIBRARY) 
 
 endif(NOT Sidekiq_FOUND)
